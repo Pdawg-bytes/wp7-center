936de4c459f94043a09d773119b052ec45296249
Pdawg11239 <83825746+Pdawg-bytes@users.noreply.github.com>
2022-10-13 18:00:49 -0400

    Update to nav view (NoBuild)

--- Windows Phone 7 ROM Manager/Windows Phone 7 ROM Manager/MainPage.xaml.cs
+++ Windows Phone 7 ROM Manager/Windows Phone 7 ROM Manager/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Core;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -12,6 +13,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using static System.Net.Mime.MediaTypeNames;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +27,25 @@ namespace Windows_Phone_7_ROM_Manager
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void mainshellnav_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            if (args.IsSettingsSelected)
+            {
+                contentFrame.Navigate(typeof(Settings));
+            }
+            else
+            {
+                var selectedItem = (Microsoft.UI.Xaml.Controls.NavigationViewItem)args.SelectedItem;
+                if (selectedItem != null)
+                {
+                    string selectedItemTag = ((string)selectedItem.Tag);
+                    string pageName = "Windows_Phone_7_ROM_Manager. " + selectedItemTag;
+                    Type pageType = Type.GetType(pageName);
+                    contentFrame.Navigate(pageType);
+                }
+            }
         }
     }
 }
