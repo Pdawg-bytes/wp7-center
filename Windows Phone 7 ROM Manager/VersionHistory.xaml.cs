c9e3ea81de937d1a852df82302eef03802847cd9
Pdawg-bytes <pdawg11239linusfan@gmail.com>
2022-10-18 22:35:23 -0400

    Adding versions to version history

--- Windows Phone 7 ROM Manager/VersionHistory.xaml.cs
+++ Windows Phone 7 ROM Manager/VersionHistory.xaml.cs
@@ -27,50 +27,66 @@ namespace Windows_Phone_7_ROM_Manager
         {
             this.InitializeComponent();
 
-            PopulateProjects();
+            PopulateVersions();
         }
-        private void PopulateProjects()
+        private void PopulateVersions()
         {
-            List<Project> Projects = new List<Project>();
+            List<Versions> VersionList = new List<Versions>();
 
-            Project newProject = new Project();
-            newProject.Name = "Project 1";
-            newProject.Activities.Add(new Activity()
-            { Name = "Activity 1"});
-            newProject.Activities.Add(new Activity()
-            { Name = "Activity 2"});
-            Projects.Add(newProject);
+            Versions newVersion = new Versions();
+            newVersion.Name = "RTM Updates";
+            newVersion.VersionCollection.Add(new VersionPropSet()
+            { Name = "7.0.7004 (RTM), Release Date: 10/29/2010" });
+            newVersion.VersionCollection.Add(new VersionPropSet()
+            { Name = "7.0.7008, Release Date: End of 2010" });
+            newVersion.VersionCollection.Add(new VersionPropSet()
+            { Name = "7.0.7390, Release Date: 4/22/2011" });
+            newVersion.VersionCollection.Add(new VersionPropSet()
+            { Name = "7.0.7392, Release Date: Mid 2011" });
+            newVersion.VersionCollection.Add(new VersionPropSet()
+            { Name = "7.0.7403, Release Date: Late 2011" });
+            VersionList.Add(newVersion);
 
-            newProject = new Project();
-            newProject.Name = "Project 2";
-            newProject.Activities.Add(new Activity()
-            { Name = "Activity A"});
-            newProject.Activities.Add(new Activity()
-            { Name = "Activity B"});
-            Projects.Add(newProject);
+            newVersion = new Versions();
+            newVersion.Name = "Mango Updates";
+            newVersion.VersionCollection.Add(new VersionPropSet()
+            { Name = "7.10.7720 (Mango RTM), Release Date: 9/27/2011" });
+            newVersion.VersionCollection.Add(new VersionPropSet()
+            { Name = "7.10.7740, Release Date: Late 2011" });
+            newVersion.VersionCollection.Add(new VersionPropSet()
+            { Name = "7.10.8107 (Refresh), Release Date: Late 2011" });
+            newVersion.VersionCollection.Add(new VersionPropSet()
+            { Name = "7.10.8112, Release Date: Early 2012" });
+            newVersion.VersionCollection.Add(new VersionPropSet()
+            { Name = "7.10.8773.98 (Tango), Release Date: June 28, 2012" });
+            newVersion.VersionCollection.Add(new VersionPropSet()
+            { Name = "7.10.8779.8, Release Date: Mid 2012" });
+            newVersion.VersionCollection.Add(new VersionPropSet()
+            { Name = "7.10.8783.12, Release Date: Late 2012" });
+            VersionList.Add(newVersion);
 
-            newProject = new Project();
-            newProject.Name = "Project 3";
-            Projects.Add(newProject);
+            newVersion = new Versions();
+            newVersion.Name = "Project 3";
+            VersionList.Add(newVersion);
 
-            cvsProjects.Source = Projects;
+            osVersions.Source = VersionList;
         }
     }
 
-    public class Project
+    public class Versions
     {
-        public Project()
+        public Versions()
         {
-            Activities = new ObservableCollection<Activity>();
+            VersionCollection = new ObservableCollection<VersionPropSet>();
         }
 
         public string Name { get; set; }
-        public ObservableCollection<Activity> Activities { get; private set; }
+        public ObservableCollection<VersionPropSet> VersionCollection { get; private set; }
     }
 
-    public class Activity
+    public class VersionPropSet
     {
         public string Name { get; set; }
-        public string Project { get; set; }
+        public string ReleaseDate { get; set; }
     }
 }
